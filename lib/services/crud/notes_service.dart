7242be291e99942afebc7ab5898f4905c9a51424
RasZarky <ubdoolrazak@gmail.com>
2024-04-17 16:05:35 +0000

    databaseNotes class created

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -17,5 +17,36 @@ class DatabaseUser{
 
 }
 
+class DatabaseNotes{
+  final int id;
+  final int userId;
+  final String text;
+  final bool isSyncedWithCloud;
+  DatabaseNotes({
+    required this.id,
+    required this.userId,
+    required this.text,
+    required this.isSyncedWithCloud,});
+
+  DatabaseNotes.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        userId = map[userIdColumn] as int,
+        text = map[textColumn] as String,
+        isSyncedWithCloud = map[isSyncedWithCloudColumn as int]
+            == 1 ? true : false ;
+
+  @override
+  String toString() => "Note, ID = $id, userId = $userId,"
+      " isSyncedWithCloud = $isSyncedWithCloud, text = $text";
+
+  @override bool operator == (covariant DatabaseNotes other) => id == other.id;
+
+  @override
+  int get hashCode => super.hashCode;
+}
+
 const idColumn = "id";
 const emailColumn = "email";
+const userIdColumn = "user_id";
+const textColumn = "text";
+const isSyncedWithCloudColumn = "is_synced_with_cloud";

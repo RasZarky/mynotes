538c234659b067ec412e7063803e3063a8ac9b08
RasZarky <ubdoolrazak@gmail.com>
2024-04-19 22:11:06 +0000

    test fixed

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
-import 'package:mynotes/views/notes_view.dart';
+import 'package:mynotes/views/notes/notes_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
 import 'login_view.dart';
 
--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -2,8 +2,8 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/services/crud/notes_service.dart';
-import '../constants/routes.dart';
-import '../enums/menu_action.dart';
+import '../../constants/routes.dart';
+import '../../enums/menu_action.dart';
 
 
 class NotesView extends StatefulWidget {
@@ -34,7 +34,7 @@ class _NotesViewState extends State<NotesView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Main UI"),
+        title: const Text("Your Notes"),
         actions: [
             PopupMenuButton<MenuAction>(
                 itemBuilder: (context){

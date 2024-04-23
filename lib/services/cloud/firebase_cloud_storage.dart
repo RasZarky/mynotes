0945c60ef4a426023c180c1a3340914166f1d325
RasZarky <ubdoolrazak@gmail.com>
2024-04-23 11:45:49 +0000

    migrating to cloud firestore

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -1,60 +1,62 @@
-import 'dart:async';
-import 'dart:html';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/services/cloud/cloud_storage_constants.dart';
 import 'package:mynotes/services/cloud/cloud_storage_exceptions.dart';
-import 'cloud_note.dart';
 
-class FirebaseCloudStorage{
-
-  final notes = FirebaseFirestore.instance.collection("note");
+class FirebaseCloudStorage {
+  final notes = FirebaseFirestore.instance.collection('notes');
 
   Future<void> deleteNote({required String documentId}) async {
-    try{
+    try {
       await notes.doc(documentId).delete();
-    } catch(e){
+    } catch (e) {
       throw CouldNotDeleteNoteException();
     }
   }
 
-  Future<void> updateNote({required String documentId, required String text})
-  async {
-    try{
+  Future<void> updateNote({
+    required String documentId,
+    required String text,
+  }) async {
+    try {
       await notes.doc(documentId).update({textFieldName: text});
-    }catch (e){
+    } catch (e) {
       throw CouldNotUpdateNoteException();
     }
   }
 
   Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) =>
-    notes.snapshots().map((event) => event.docs.map((doc) => 
-        CloudNote.fromSnapshot(doc)).where((note) =>
-    note.ownerUserId == ownerUserId)
-    );
-  
+      notes.snapshots().map((event) => event.docs
+          .map((doc) => CloudNote.fromSnapshot(doc))
+          .where((note) => note.ownerUserId == ownerUserId));
+
   Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
-    try{
-      return notes.where(
+    try {
+      return await notes
+          .where(
         ownerUserIdFieldName,
         isEqualTo: ownerUserId,
-      ) .get()
-          .then((value) => value.docs.map((doc){
-            return CloudNote(
-                documentId: doc.id,
-                ownerUserId: doc.data()[ownerUserIdFieldName] as String,
-                text: doc.data()[textFieldName] as String,
-            );
-      }));
-    } catch(e){
+      )
+          .get()
+          .then(
+            (value) => value.docs.map((doc) => CloudNote.fromSnapshot(doc)),
+      );
+    } catch (e) {
       throw CouldNotGetAllNotesException();
     }
   }
 
-  void createNote({required String ownerUserId}) async {
-    notes.add({
+  Future<CloudNote> createNewNote({required String ownerUserId}) async {
+    final document = await notes.add({
       ownerUserIdFieldName: ownerUserId,
-      textFieldName: "",
+      textFieldName: '',
     });
+    final fetchedNote = await document.get();
+    return CloudNote(
+      documentId: fetchedNote.id,
+      ownerUserId: ownerUserId,
+      text: '',
+    );
   }
 
   static final FirebaseCloudStorage _shared =
--- lib/services/crud/crud_exception.dart
+++ lib/services/crud/crud_exception.dart
@@ -1,11 +1,11 @@
-class DatabaseAlreadyOpenException implements Exception{}
-class UnableToGetDocumentDirectoryException implements Exception{}
-class DatabaseNotOpenException implements Exception{}
-class CouldNotDeleteUser implements Exception{}
-class UserAlreadyExist implements Exception{}
-class CouldNotFindUser implements Exception{}
-class CouldNotDeleteNote implements Exception{}
-class CouldNotFindNote implements Exception{}
-class CouldNotUpdateNote implements Exception{}
-class UserShouldBeSetBeforeReadingAllNotes implements Exception{}
-
+// class DatabaseAlreadyOpenException implements Exception{}
+// class UnableToGetDocumentDirectoryException implements Exception{}
+// class DatabaseNotOpenException implements Exception{}
+// class CouldNotDeleteUser implements Exception{}
+// class UserAlreadyExist implements Exception{}
+// class CouldNotFindUser implements Exception{}
+// class CouldNotDeleteNote implements Exception{}
+// class CouldNotFindNote implements Exception{}
+// class CouldNotUpdateNote implements Exception{}
+// class UserShouldBeSetBeforeReadingAllNotes implements Exception{}
+//
